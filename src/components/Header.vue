1f6156d6af2891d8917eaef68133175bad1241da
oat431 <oat431@gmail.com>
2022-03-09 17:51:53 +0700

    [💄 update] : css plugin using tailwind

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- component -->
+  <header>
+    <div
+      class="w-full bg-cover bg-center"
+      style="
+        height: 20rem;
+        background-image: url(https://img.freepik.com/free-vector/food-cooking-graphic-illustration_53876-9120.jpg?size=626&ext=jpg);
+      "
+    >
+      <div
+        class="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50"
+      >
+        <div class="text-center">
+          <h1
+            class="text-gray-200 text-2xl font-semibold uppercase md:text-3xl"
+          >
+            Recipe WebSite
+          </h1>
+          <div class="text-gray-300 text-md font-semibold md:text-md">
+            <p>Search what the suitable recipe for you</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
